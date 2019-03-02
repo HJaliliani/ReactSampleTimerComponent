679e39a53a74b51280e63e8b2f11b827f727e5b1
Hamed Jaliliani <jalilian_ham@yahoo.com>
2019-03-02 15:17:59 +0330

    Timer Element

--- src/index.js
+++ src/index.js
@@ -4,9 +4,26 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const name =' JSX element' ;
+const elemtSample = <h1>Test Elemnt {name} </h1> ;
+function tick(){
+const elementDate = ( 
+    <div>
+        <h1>H1</h1>
+        <h2> It is  {new Date().toLocaleTimeString()}. </h2>
+    </div>
+);
+ReactDOM.render( elementDate, document.getElementById('root'));
+}
+
+setInterval(tick, 100)
+//var element = React.createElement('h1',  {className:'greeting'} , 'Hello First offline  React App')
+//ReactDOM.render(element, document.getElementById('root'));
+//ReactDOM.render(elementDate, document.getElementById('root')) ;
+
+//ReactDOM.render(elemtSample, document.getElementById('root')) ;
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
